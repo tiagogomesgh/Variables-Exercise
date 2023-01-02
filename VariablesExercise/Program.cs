6f218409e58952309520792a82b9c5b606b80b01
tiagogomesgh <tiagogomesfl@outlook.com>
2023-01-01 22:02:32 -0800

    Completed Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Patient Information:");
+            Console.WriteLine($"Name: {myName}  |  Age: {myAge}  |  Gender: {myGender}");
+            Console.WriteLine($"Organ Donor: {isOrganDonor}  |  Blinks/Min: {blinksPerMinute}  |  Blinks/Sec : {blinksPerSecond}");
         }
+
+        static string myName = "Tiago";
+
+        static int myAge = 21;
+
+        static char myGender = 'M';
+
+        static bool isOrganDonor = true;
+
+        static double blinksPerMinute = 76.2;
+
+        static decimal blinksPerSecond = .4M;
+
     }
 }
